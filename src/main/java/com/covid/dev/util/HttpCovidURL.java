0d8b47328bad2c3ec330a418e2314dbbad78d322
zaerbib <ilunga.joel@yahoo.fr>
2022-06-07 21:45:14 +0200

    configuration

--- src/main/java/com/covid/dev/util/HttpCovidURL.java
+++ src/main/java/com/covid/dev/util/HttpCovidURL.java
@@ -1,14 +1,14 @@
 package com.covid.dev.util;
 
 public class HttpCovidURL {
-	public final static String COVID_HOSP_TXAD_AGE_FRA = "https://static.data.gouv.fr/resources/donnees-hospitalieres-relatives-a-lepidemie-de-covid-19/20220603-190212/covid-hosp-txad-age-fra-2022-06-03-19h02.csv";
-	public final static String COVID_HOSP_TXAD_REG = "https://static.data.gouv.fr/resources/donnees-hospitalieres-relatives-a-lepidemie-de-covid-19/20220603-190211/covid-hosp-txad-reg-2022-06-03-19h02.csv";
-	public final static String COVID_HOSP_TXAD_FRA = "https://static.data.gouv.fr/resources/donnees-hospitalieres-relatives-a-lepidemie-de-covid-19/20220603-190209/covid-hosp-txad-fra-2022-06-03-19h02.csv";
-	public final static String COVID_HOSP_AD_AGE = "https://static.data.gouv.fr/resources/donnees-hospitalieres-relatives-a-lepidemie-de-covid-19/20220602-190834/covid-hosp-ad-age-2022-06-02-19h08.csv";
-	public final static String COVID_HOSPIT_INCID_REG = "https://static.data.gouv.fr/resources/donnees-hospitalieres-relatives-a-lepidemie-de-covid-19/20220603-190202/covid-hospit-incid-reg-2022-06-03-19h02.csv";
-	public final static String COVID_HOSPIT = "https://static.data.gouv.fr/resources/donnees-hospitalieres-relatives-a-lepidemie-de-covid-19/20220603-190204/covid-hospit-2022-06-03-19h02.csv";
-	public final static String COVID_HOSPIT_INCID = "https://static.data.gouv.fr/resources/donnees-hospitalieres-relatives-a-lepidemie-de-covid-19/20220603-190205/covid-hospit-incid-2022-06-03-19h02.csv";
-	public final static String COVID_HOSPIT_CLAGE10 = "https://static.data.gouv.fr/resources/donnees-hospitalieres-relatives-a-lepidemie-de-covid-19/20220603-190208/covid-hospit-clage10-2022-06-03-19h02.csv";
-	public final static String COVID_HOSPIT_ETAB = "https://static.data.gouv.fr/resources/donnees-hospitalieres-relatives-a-lepidemie-de-covid-19/20220603-190204/covid-hospit-etab-2022-06-03-19h02.csv";
+	public final static String COVID_HOSP_TXAD_AGE_FRA = "https://www.data.gouv.fr/fr/datasets/r/e3d83ab3-dc52-4c99-abaf-8a38050cc68c";
+	public final static String COVID_HOSP_TXAD_REG = "https://www.data.gouv.fr/fr/datasets/r/46f145d4-9607-45a0-bc3c-86241136ca24";
+	public final static String COVID_HOSP_TXAD_FRA = "https://www.data.gouv.fr/fr/datasets/r/fe3e7099-a975-4181-9fb5-2dd1b8f1b552";
+	public final static String COVID_HOSP_AD_AGE = "https://www.data.gouv.fr/fr/datasets/r/dc7663c7-5da9-4765-a98b-ba4bc9de9079";
+	public final static String COVID_HOSPIT_INCID_REG = "https://www.data.gouv.fr/fr/datasets/r/a1466f7f-4ece-4158-a373-f5d4db167eb0";
+	public final static String COVID_HOSPIT = "https://www.data.gouv.fr/fr/datasets/r/63352e38-d353-4b54-bfd1-f1b3ee1cabd7";
+	public final static String COVID_HOSPIT_INCID = "https://www.data.gouv.fr/fr/datasets/r/6fadff46-9efd-4c53-942a-54aca783c30c";
+	public final static String COVID_HOSPIT_CLAGE10 = "https://www.data.gouv.fr/fr/datasets/r/08c18e08-6780-452d-9b8c-ae244ad529b3";
+	public final static String COVID_HOSPIT_ETAB = "https://www.data.gouv.fr/fr/datasets/r/41b9bd2a-b5b6-4271-8878-e45a8902ef00";
 	
 }
